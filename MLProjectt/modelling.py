6f7f3eb740b37b2bff00b9770b0b2117e28420b9
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-05-29 20:30:23 +0700

    update

--- MLProjectt/modelling.py
+++ MLProjectt/modelling.py
@@ -1,32 +1,41 @@
+import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score
-import joblib
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-# mlflow.set_tracking_uri("http://localhost:8000")
 
-# Set experiment (jika belum ada, akan dibuat)
-mlflow.set_experiment("iris-logistic-regression")
-
+df = pd.read_csv("../preprocessing/titanic_preprocessed_train.csv")
 mlflow.sklearn.autolog()
+df = df.drop(columns=["Name", "Ticket", "Cabin", "PassengerId"], errors='ignore')
 
-X_train = joblib.load("../Workflow-CI/iris_preproceccing/X_train.pkl")
-X_test = joblib.load("../Workflow-CI/iris_preproceccing/X_test.pkl")
-y_train = joblib.load("../Workflow-CI/iris_preproceccing/y_train.pkl")
-y_test = joblib.load("../Workflow-CI/iris_preproceccing/y_test.pkl")
+label_cols = df.select_dtypes(include='object').columns
+for col in label_cols:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col].astype(str))
+
+X = df.drop(columns=["Survived"])
+y = df["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 with mlflow.start_run():
-    model = LogisticRegression(C=0.5, solver='liblinear')
+    model = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
-
+    
     y_pred = model.predict(X_test)
-
+    
     accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='macro')
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
     mlflow.log_metric("accuracy", accuracy)
     mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
 
-    print("Model dilatih dan dicatat secara otomatis di MLflow dengan metrik.")
-
+    print(f"✅ Model dilatih dan dicatat di MLflow.")
+    print(f"🔍 Akurasi: {accuracy:.4f}")
