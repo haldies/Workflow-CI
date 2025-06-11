31f9028a9a5ea1ea0d9cba970750d9da0b4e549c
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-06-11 15:00:28 +0700

    update konfigurasi

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -9,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
-mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")  
 mlflow.set_experiment("ci-training")
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
