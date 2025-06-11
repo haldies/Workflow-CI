8f27bc41df2a106096e6ddc332df19fd1470f779
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-06-12 05:57:20 +0700

    debugin folder v8

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,17 +6,16 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from mlflow.models.signature import infer_signature
 
-
-
-mlflow.set_experiment("titanic_experiment")
-base_dir = os.path.dirname(os.path.abspath(__file__))
-mlruns_path = os.path.join(base_dir, "mlruns")
-mlflow.set_tracking_uri(f"file://{mlruns_path}")
 base_dir = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(base_dir, "titanic_preprocessing", "titanic_preprocessed_train.csv")
-mlflow.sklearn.autolog()
+
 df = pd.read_csv(csv_path)
+
+mlflow.set_experiment("titanic_experiment")
+
+
 
 df = df.drop(columns=["Name", "Ticket", "Cabin", "PassengerId", "Ticket_number", "Ticket_item"], errors='ignore')
 
@@ -33,14 +32,39 @@ print("Jumlah fitur:", X.shape[1])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression(max_iter=200)
-model.fit(X_train, y_train)
+with mlflow.start_run() as run:
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
-print(f"✅ Model dilatih dan dicatat di MLflow.")
-print(f"🔍 Akurasi: {accuracy:.4f}")
+
+    mlflow.log_param("model_type", "Logistic Regression")
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
+
+    signature = infer_signature(X_train, model.predict(X_train))
+
+    
+    input_example = X_train.head(5)
+
+    
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        signature=signature,
+        input_example=input_example
+    )
+
+    print(f"Akurasi: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1 Score: {f1}")
+
+    print("Run ID:", run.info.run_id)
